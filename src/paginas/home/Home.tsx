29d63a196cbaab370317df54bab6ec1d429ec0c8
Amanda Falcão <amandaf.dias96@gmail.com>
2022-08-25 13:36:53 -0300

    Projeto em React criado.

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <>
+      <h1 className='titulo'>Home</h1>
+      <img src="https://avatars.githubusercontent.com/u/65048587?v=4" alt="Desenho de uma menina de óculos" className='img'/>
+    </>
+  );
+}
+
+export default Home;
