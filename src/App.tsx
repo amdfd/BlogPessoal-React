57c2f252ec54ec87ab65967d3579fe14d04cafc3
Amanda Falcão <amandaf.dias96@gmail.com>
2022-08-26 18:51:01 -0300

    Componentes Footer e Navbar criados

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import Home from './paginas/home/Home';
-import './App.css';
+import Home from "./paginas/home/Home";
+import Navbar from "./components/estaticos/navbar/Navbar";
+import Footer from "./components/estaticos/footer/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <Home />
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
   );
 }
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import './Home.css';
+import "./Home.css";
+import { Grid, Button, Typography } from "@material-ui/core";
+import { Box } from "@mui/material";
+
 
 function Home() {
   return (
     <>
-      <h1 className='titulo'>Bem-vindes!</h1>
-      <img src="https://i.imgur.com/1RwWtgp.png" alt="Desenho de uma menina de óculos" className='img'/>
+      <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem-vinde!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>Blog pessoal com meus estudos sobre linguagens de programação e tecnologias.</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/1RwWtgp.png" alt="" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
     </>
   );
 }
